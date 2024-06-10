fe657794a2e27ca0b4ae20180456e9cff4cdc227
andisugandi <andisugandi@gmail.com>
2024-06-10 09:30:38 +0700

    Generate Category Model and AddCategoryIdToTasks Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_014649) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_123908) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_014649) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
 end
