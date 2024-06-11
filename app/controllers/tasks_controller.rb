a5ae19c32cf63f67985e42d5d4ed6c90936a8de4
andisugandi <andisugandi@gmail.com>
2024-06-12 06:57:54 +0700

    Add full set of actions on Tasks and Categories controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,24 @@ class TasksController < ApplicationController
     @tasks = ['Jemur cucian baju', 'Sapu teras rumah', 'Cuci piring', 'Memotong rumput']
   end
 
+  def show
+  end
+
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
   end
 end
