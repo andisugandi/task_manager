f7b692b6bc1052176b5ac595a58095f5b564eecc
andisugandi <andisugandi@gmail.com>
2024-06-25 10:26:44 +0700

    Rearrange routes and generate Access controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  root 'main#index'
+
+  root 'tasks#index'
+
+  get 'menu' => 'access#menu'
+  get 'login' => 'access#new'
+  delete 'logout' => 'access#destroy'
+  resource :access, controller: 'access', only: [:new, :create, :destroy] do
+    member do
+      get :menu
+    end
+  end
 
   match 'about', to: 'main#about', via: :get
-  match 'hello', to: 'main#hello', via: :get
-
-  get 'main/index'
 
   resources :categories do
     member do
